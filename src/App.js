2bd9823e1f464a65f3400e8d3b7466b46c210b36
Nicholas Interest <NicholasInterest1@users.noreply.github.com>
2019-09-09 17:15:07 -0400

    Did stretch after all

--- src/App.js
+++ src/App.js
@@ -17,17 +17,23 @@ function App() {
     setCart([...cart, item]);
   };
 
+  const removeItem = (id) => {
+    setCart([...cart].filter(item => item.id !== id));
+  };
+
   return (
     <div className="App">
-      <ProductContext.Provider value={{ products, addItem }}>
+      <ProductContext.Provider value={{ products, addItem, removeItem }}>
         <CartContext.Provider value={cart}>
-          <Navigation cart={cart} />
+          <Navigation />
 
           {/* Routes */}
 
-          <Route exact path="/" component={Products} />
+          <Route exact path="/" component={Products} 
+		  />
 
-          <Route path="/cart" render={() => <ShoppingCart cart={cart} />} />
+          <Route path="/cart" component={ShoppingCart} 
+		  />
         </CartContext.Provider>
       </ProductContext.Provider>
     </div>
